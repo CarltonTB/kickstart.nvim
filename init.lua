c736d339c83e29acad44bd3194bd6d3577e2ab5f
Carlton Brady <ctombrady@gmail.com>
2024-07-18 23:02:21 -0400

    add cssls

--- init.lua
+++ init.lua
@@ -593,6 +593,7 @@ require('lazy').setup({
             },
           },
         },
+        cssls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
