7daff33bd988a2cdabd55dab80a1231922fa3eea
Carlton Brady <cbrady@ycharts.com>
2024-10-21 13:36:11 -0400

    add ruff lsp for linting messages

--- init.lua
+++ init.lua
@@ -641,6 +641,7 @@ require('lazy').setup({
             },
           },
         },
+        ruff_lsp = {},
         cssls = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
