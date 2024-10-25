9a35a212f7191f395db04b3ba180bd1699f991d4
Carlton Brady <cbrady@ycharts.com>
2024-10-25 14:48:25 -0400

    add jsonls

--- init.lua
+++ init.lua
@@ -643,6 +643,7 @@ require('lazy').setup({
         },
         ruff_lsp = {},
         cssls = {},
+        jsonls = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
